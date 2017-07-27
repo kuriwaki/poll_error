533967ca4a4df39ade56e8ed982226fb2c1fd429
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-07-27 13:46:33 -0400

    create cv of weights

--- 03_tabulate_polls.R
+++ 03_tabulate_polls.R
@@ -14,7 +14,9 @@ tab_cc <- cc_raw %>%
   summarize(cces_n_raw = n(),
             cces_n_voters = sum(turnout_wgt, na.rm = TRUE),
             cces_tothrc_raw = sum(vote_hrc, na.rm = TRUE),
-            cces_tothrc_adj_trn = sum(vote_hrc*turnout_wgt, na.rm = TRUE)) %>%
+            cces_tothrc_adj_trn = sum(vote_hrc*turnout_wgt, na.rm = TRUE),
+            sd_turnout_wgt = sqrt(sum((turnout_wgt - mean(turnout_wgt))^2)/n()),
+            cv_turnout_wgt = sd_turnout_wgt / mean(turnout_wgt)) %>%
   mutate(cces_pct_hrc_raw = cces_tothrc_raw / cces_n_raw,
          cces_pct_hrc_vep = cces_tothrc_adj_trn / cces_n_raw,
          cces_pct_hrc_voters = cces_tothrc_adj_trn / cces_n_voters)
--- 04_join_build.R
+++ 04_join_build.R
@@ -22,6 +22,7 @@ df <- df_joined %>%
          cces_n_voters, cces_n_raw,
          cces_tothrc_adj_trn, cces_tothrc_raw, 
          cces_pct_hrc_voters, cces_pct_hrc_vep, cces_pct_hrc_raw,
+         cv_turnout_wgt,
          yougov_pct_hrc, yougov_n,
          `State Results Website`)
 
