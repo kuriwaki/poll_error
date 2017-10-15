f7eb7acde4face9c213c5939cca7bfabe1815837
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-10-15 13:52:34 -0400

    add partisan undecideds for other populations

--- 03_tabulate_polls.R
+++ 03_tabulate_polls.R
@@ -16,9 +16,12 @@ tab_cc <- cc_raw %>%
             cces_n_voters = sum(turnout_wgt, na.rm = TRUE),
             cces_n_vv = sum(vv_turnout, na.rm = TRUE),
             cces_n_post_voters = sum(post_turnout, na.rm = TRUE),
+            
             cces_tothrc_raw = sum(vote_hrc_pre, na.rm = TRUE),
             cces_totdjt_raw = sum(vote_djt_pre, na.rm = TRUE),
             cces_totund_raw = sum(vote_und_pre, na.rm = TRUE),
+            cces_demund_raw = sum(vote_und_pre*(pid3 == 1), na.rm = TRUE),
+            cces_repund_raw = sum(vote_und_pre*(pid3 == 2), na.rm = TRUE),
             
             cces_tothrc_raw_post = sum(vote_hrc_post, na.rm = TRUE),
             cces_totdjt_raw_post = sum(vote_djt_post, na.rm = TRUE),
@@ -26,32 +29,42 @@ tab_cc <- cc_raw %>%
             cces_tothrc_adj_trn = sum(vote_hrc_pre*turnout_wgt, na.rm = TRUE),
             cces_totdjt_adj_trn = sum(vote_djt_pre*turnout_wgt, na.rm = TRUE),
             cces_totund_adj_trn = sum(vote_und_pre*turnout_wgt, na.rm = TRUE),
-            
             cces_demund_adj_trn = sum(vote_und_pre*turnout_wgt*(pid3 == 1), na.rm = TRUE),
             cces_repund_adj_trn = sum(vote_und_pre*turnout_wgt*(pid3 == 2), na.rm = TRUE),
             
             cces_tothrc_vv = sum(vote_hrc_pre*vv_turnout, na.rm = TRUE),
             cces_totdjt_vv = sum(vote_djt_pre*vv_turnout, na.rm = TRUE),
             cces_totund_vv = sum(vote_und_pre*vv_turnout, na.rm = TRUE),
+            cces_demund_vv = sum(vote_und_pre*vv_turnout*(pid3 == 1), na.rm = TRUE),
+            cces_repund_vv = sum(vote_und_pre*vv_turnout*(pid3 == 2), na.rm = TRUE),
+            
             sd_turnout_wgt = sqrt(sum((turnout_wgt - mean(turnout_wgt))^2)/n()),
             cv_turnout_wgt = sd_turnout_wgt / mean(turnout_wgt)) %>%
   mutate(cces_pct_hrc_raw = cces_tothrc_raw / cces_n_raw,
          cces_pct_hrcund_raw = (cces_tothrc_raw + cces_totund_raw) / cces_n_raw,
+         cces_pct_hrcdund_raw = (cces_tothrc_raw + cces_demund_raw) / cces_n_raw,
+         
          cces_pct_hrc_vep = cces_tothrc_adj_trn / cces_n_raw,
          cces_pct_hrc_voters = cces_tothrc_adj_trn / cces_n_voters,
          cces_pct_hrcund_voters = (cces_tothrc_adj_trn + cces_totund_adj_trn) / cces_n_voters,
          cces_pct_hrcdund_voters = (cces_tothrc_adj_trn + cces_demund_adj_trn) / cces_n_voters,
+         
          cces_pct_hrc_vv = cces_tothrc_vv / cces_n_vv,
          cces_pct_hrcund_vv = (cces_tothrc_vv + cces_totund_vv) / cces_n_vv,
+         cces_pct_hrcdund_vv = (cces_tothrc_vv + cces_demund_vv) / cces_n_vv,
          cces_pct_hrc_voters_post = cces_tothrc_raw_post / cces_n_voters) %>%
   mutate(cces_pct_djt_raw = cces_totdjt_raw / cces_n_raw,
          cces_pct_djtund_raw = (cces_totdjt_raw + cces_totund_raw) / cces_n_raw,
+         cces_pct_djtrund_raw = (cces_totdjt_raw + cces_repund_raw) / cces_n_raw,
+         
          cces_pct_djt_vep = cces_totdjt_adj_trn / cces_n_raw,
          cces_pct_djt_voters = cces_totdjt_adj_trn / cces_n_voters,
          cces_pct_djtund_voters = (cces_totdjt_adj_trn + cces_totund_adj_trn) / cces_n_voters,
          cces_pct_djtrund_voters = (cces_totdjt_adj_trn + cces_repund_adj_trn) / cces_n_voters,
+         
          cces_pct_djt_vv = cces_totdjt_vv / cces_n_vv,
          cces_pct_djtund_vv = (cces_totdjt_vv + cces_totund_vv) / cces_n_vv,
+         cces_pct_djtrund_vv = (cces_totdjt_vv + cces_repund_vv) / cces_n_vv,
          cces_pct_djt_voters_post = cces_totdjt_raw_post / cces_n_voters)
 tab_cc
 
